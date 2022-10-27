fa65277c193c1e9381a9bf5802f4a6a2bdf3e346
Samy Rahmani <samy@localhost.localdomain>
2022-10-27 11:25:03 +0000

    Changed v1 route to api & added 404 error handeling

--- RandomMessage/BackEnd/api/app.js
+++ RandomMessage/BackEnd/api/app.js
@@ -6,7 +6,20 @@ const v1 = require("./v1/V1Router");
 const app = express();
 app.use(express.json());
 
-app.use('v1', v1);
+app.use('/api/', v1);
+
+app.use((req, res, next) => {
+  res.status(404);
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 app.listen(process.env.PORT, () => {
   log(`Started listening on PORT ${process.env.PORT}`);
