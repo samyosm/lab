959f9cb0848711e2540b8e61607435516ed2418b
Samy Rahmani <samy@samy.localdomain>
2022-10-26 21:34:05 -0400

    Removed Prefix from RandomMessage

--- RandomMessage/BackEnd/api/app.js
+++ RandomMessage/BackEnd/api/app.js
@@ -6,7 +6,7 @@ const v1 = require("./v1/V1Router");
 const app = express();
 app.use(express.json());
 
-app.use(process.env.API_PREFIX + 'v1', v1);
+app.use('v1', v1);
 
 app.listen(process.env.PORT, () => {
   log(`Started listening on PORT ${process.env.PORT}`);
